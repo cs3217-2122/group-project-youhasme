e11034d9fbabb5231d6367adf99cc2a0fce74093
Wayne Tee <waynetee37@gmail.com>
2022-03-20 10:33:48 +0800

    Add support for PUSH

--- YouHasMe/YouHasMe/models/GameEngine/MoveMechanic.swift
+++ YouHasMe/YouHasMe/models/GameEngine/MoveMechanic.swift
@@ -15,19 +15,48 @@ struct MoveMechanic: GameMechanic {
     //  - levelLayer: Current game state
     // Returns a map of coordinates (y, x, position in tile) of entities to their actions
     func apply(update: UpdateAction, levelLayer: LevelLayer) -> [[Int]: [EntityAction]] {
-        var dict: [[Int]: [EntityAction]] = [:]
-        let (dx, dy) = update.getMovement()
+        // Get coordinates of YOU blocks
+        var coordsOfYou: Set<[Int]> = []
         for r in 0..<levelLayer.dimensions.height {
             for c in 0..<levelLayer.dimensions.width {
                 let entities = levelLayer.getTileAt(x: c, y: r).entities
                 for i in 0..<entities.count where entities[i].activeBehaviours.contains(.property(.you)) {
-                    if levelLayer.isWithinBounds(x: c + dx, y: r + dy) {
-                        dict[[r, c, i]] = [.move(dx, dy)]
-                    }
+                    coordsOfYou.insert([r, c, i])
                 }
             }
         }
-        return dict
+
+        // Get coordinates of blocks that will move
+        var coordsWillMove: Set<[Int]> = []
+        let (dx, dy) = update.getMovement()
+        for coords in coordsOfYou {  // For each you block
+            var coordsMightMove: Set<[Int]> = [coords]  // Coordinates of YOU blocks or blocks that are pushed
+            var curY = coords[0] + dy
+            var curX = coords[1] + dx
+            while levelLayer.isWithinBounds(x: curX, y: curY) {  // While we have space to move
+                let entities = levelLayer.getTileAt(x: curX, y: curY).entities
+                var foundPushable = false
+                // Add pushable entities
+                for i in 0..<entities.count where entities[i].activeBehaviours.contains(.property(.push)) {
+                    coordsMightMove.insert([curY, curX, i])
+                    foundPushable = true
+                }
+
+                if !foundPushable {  // Empty space found, can move chain of blocks
+                    coordsWillMove = coordsWillMove.union(coordsMightMove)
+                }
+
+                curY += dy
+                curX += dx
+            }
+        }
+
+        // Return map of coordinates to move action
+        var actions: [[Int]: [EntityAction]] = [:]
+        for coords in coordsWillMove {
+            actions[coords] = [.move(dx, dy)]
+        }
+        return actions
     }
 
 }
--- YouHasMe/YouHasMeTests/GameEngineTests.swift
+++ YouHasMe/YouHasMeTests/GameEngineTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class GameEngineTests: XCTestCase {
 
-    func test() throws {
+    func testSimple() throws {
         var levelLayer = LevelLayer(dimensions: Rectangle(width: 5, height: 5))
         levelLayer.add(entity: Entity(entityType: EntityTypes.Nouns.baba), x: 0, y: 0)
         levelLayer.add(entity: Entity(entityType: EntityTypes.Verbs.vIs), x: 1, y: 0)
@@ -22,5 +22,27 @@ class GameEngineTests: XCTestCase {
         gameEngine.update(action: .moveDown)
         print(gameEngine.levelLayer)
         XCTAssertEqual(gameEngine.levelLayer.getTileAt(x: 2, y: 4).entities.count, 1)
+    }
+
+    func testComplex() throws {
+        var levelLayer = LevelLayer(dimensions: Rectangle(width: 5, height: 5))
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Nouns.baba), x: 0, y: 0)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Verbs.vIs), x: 1, y: 0)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Properties.you), x: 2, y: 0)
+
+        levelLayer.add(entity: Entity(entityType: EntityTypes.NounInstances.baba), x: 2, y: 1)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Properties.you), x: 3, y: 1)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.NounInstances.baba), x: 4, y: 1)
+
+        levelLayer.add(entity: Entity(entityType: EntityTypes.NounInstances.baba), x: 2, y: 2)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Properties.you), x: 3, y: 2)
+        levelLayer.add(entity: Entity(entityType: EntityTypes.Properties.you), x: 4, y: 2)
+
+        levelLayer = RuleEngine().applyRules(to: levelLayer)
+        print(levelLayer)
+        var gameEngine = GameEngine(levelLayer: levelLayer)
+        gameEngine.update(action: .moveRight)
+        print(gameEngine.levelLayer)
+
     }
 }
