da0071fbe2e749a8896e7516567c38a43408013d
Neo Jing Xuan <neojxuan@gmail.com>
2022-04-16 17:01:21 +0800

    update achievement images

--- YouHasMe/YouHasMe/views/Achievements/Views/AchievementImages.swift
+++ YouHasMe/YouHasMe/views/Achievements/Views/AchievementImages.swift
@@ -22,8 +22,8 @@ struct AchievementImages {
             return "creativity"
         case "Speedy Game":
             return "speedy_game"
-        case "You are Gogo":
-            return "you_are_gogo"
+        case "GOGO IS YOU":
+            return "gogo_is_you"
         default:
             return "question"
         }
