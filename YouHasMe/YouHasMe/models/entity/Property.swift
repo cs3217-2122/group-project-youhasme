f321bd1a62f0825a5df58966f2bb5c4094a8470f
dhshah1 <19668322+dhshah1@users.noreply.github.com>
2022-04-17 22:08:36 +0800

    remove pull and defeat

--- YouHasMe/YouHasMe/models/entity/Property.swift
+++ YouHasMe/YouHasMe/models/entity/Property.swift
@@ -5,10 +5,8 @@ enum Property: Hashable {
     case win
     case stop
     case push
-    case pull
     case player(Int)
     case sink
-    case defeat
 }
 
 extension Property: CustomDebugStringConvertible {
@@ -18,14 +16,10 @@ extension Property: CustomDebugStringConvertible {
             return "you"
         case .win:
             return "win"
-        case .defeat:
-            return "defeat"
         case .stop:
             return "stop"
         case .push:
             return "push"
-        case .pull:
-            return "pull"
         case .player(let num):
             return "player\(num)"
         case .sink:
